c21f3b118fe54c92434b685313fab882128aa23f
bekirbilgec <bekirbilgec42@gmail.com>
2023-02-05 16:06:43 +0000

    added Nexus server terraform files

--- msp-26-nexus-server-template/nexus-vars.tf
+++
@@ -1,8 +0,0 @@
-variable "mykey" {}
-variable "ami" {
-  description = "amazon linux 2 ami"
-}
-variable "region" {}
-variable "instance_type" {}
-variable "nexus_server_secgr" {}
-variable "nexus-server-tag" {}
